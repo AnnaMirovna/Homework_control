4156c70db84b9dc12c3ca86eeb90ee04e364b57a
Anna Yakovleva <89216575526@mail.ru>
2023-02-25 21:56:56 +0300

    Method to create an array of strings with elements less than or equel to three

--- Program.cs
+++ Program.cs
@@ -9,3 +9,17 @@ string[] ArrayString(int value)
     }
     return array1;
 }
+string[] SecondArrayThreeElements(string[] array1)
+{
+    string[] array2 = new string[array1.Length];
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+    return array2;
+}
